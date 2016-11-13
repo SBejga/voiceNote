1d2c014ddbba697e562aec46d0282947a7e9561c
mmuth <michaelmuth.mm@googlemail.com>
2016-11-13 13:46:21 +0100

    adsfasdfasdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import { AudioProvider } from 'ionic-audio/dist/ionic-audio';
+
 
 import { RecordAudio } from '../pages/recordAudio/recordAudio.component';
 import { PlayAudio } from '../pages/playAudio/playAudio.component';
@@ -42,3 +44,7 @@ export class MyApp {
     this.nav.setRoot(page.component);
   }
 }
+ionicBootstrap(MyApp, [provide(AudioProvider,  { useFactory: AudioProvider.factory })], {
+
+});  
+  
